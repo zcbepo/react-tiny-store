936248b08a1f10a1864ac39ab5201c3fb14781c5
zoucheng <zcbepo@gmail.com>
2022-06-11 00:38:21 +0800

    fix: change entry file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV !== 'production') {
     module.exports = require('./lib/store.js')
 } else {
     module.exports = require('./lib/store.prod.js')
